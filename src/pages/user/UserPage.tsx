7d46305110387900995ab09fee526cec6f9b382d
MotoIluyha <motoilyuha@mail.ru>
2024-09-23 16:51:03 +0300

    Add UploadAvatarWidget
    Signed-off-by: MotoIluyha <motoilyuha@mail.ru>

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -1,9 +1,30 @@
+import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
+
+import {Divider, Space} from "antd";
+
+import {Tables} from "../../types/supabase.ts";
+import {getUserByLogin} from "../../features/SupaBaseUsers.ts";
+import UploadAvatar from "../../widgets/User/UploadAvatarWidget.tsx";
+
 
 export default function UserPage() {
   const {user_login} = useParams();
+  const [person, setPerson] = useState<Tables<'users'>>();
+
+  useEffect(() => {
+    if (user_login) {
+      getUserByLogin(user_login)
+        .then(person_by_login => {
+          setPerson(person_by_login);
+        })
+        .catch(e => console.error(e))
+    }
+  }, [user_login])
 
   return (
-    <h1>User Page {user_login}</h1>
+    <Space align='center' direction='horizontal' size='large' split={<Divider type="vertical"/>}>
+      {person && <UploadAvatar person={person}/>}
+    </Space>
   )
 }
