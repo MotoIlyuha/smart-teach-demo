981777f5bfeb3c75e8e5c6a6753e62312d853fcd
MotoIluyha <motoilyuha@mail.ru>
2024-09-24 16:50:02 +0300

    Add BreadcrumbWidget for dev mode
    Signed-off-by: MotoIluyha <motoilyuha@mail.ru>

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,14 +1,19 @@
 import {Outlet} from "react-router-dom";
-import {App, Layout} from "antd";
+import {App, Layout, theme} from "antd";
 
 import NavBar from "../widgets/NavBar/NavBar.tsx";
+import BreadcrumbWidget from "../widgets/Dev/BreadcrumbWidget.tsx";
 
+import {dev_mode, max_message_count} from "../config/allConfig.ts";
 import styles from "../styles/layout.module.css";
-import {max_message_count} from "../config/allConfig.ts";
 
 const {Content, Footer, Header} = Layout;
 
 export default function RootLayout() {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
     <App message={{maxCount: max_message_count}} notification={{placement: 'bottomRight'}}>
       <Layout className={styles.layout}>
@@ -16,7 +21,10 @@ export default function RootLayout() {
           <NavBar/>
         </Header>
         <Content className={styles.layoutContent}>
-          <Outlet/>
+          {dev_mode && <BreadcrumbWidget/>}
+          <Layout style={{ padding: '24px 0', background: colorBgContainer, borderRadius: borderRadiusLG }}>
+            <Outlet/>
+          </Layout>
         </Content>
         <Footer className={styles.layoutFooter}>
           SmartTeach ©{new Date().getFullYear()} | Created by <a href={'https://github.com/MotoIlyuha'}>MotoIlyuha</a>
